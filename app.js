f65617edfc72f556585198ee73ab36bd64c5deb9
celveza <adrielip2007@gmail.com>
2024-04-05 12:03:01 +0200

    I'm trying to do things but none of them works

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
+// Variables
+
+
+
+
+// Functions
+
+
 //Create grid at start
 document.addEventListener("DOMContentLoaded", function(){
-    newGrid(16)
+    newGrid(16,"#000000")
+    draw()
 });
 
 //Create the grid
-function newGrid(size) {
+
+function newGrid(size, color) {
     let grid = document.querySelector(".grid");
     grid.style.gridTemplateRows = `repeat(${size},1fr)`;
     grid.style.gridTemplateColumns = `repeat(${size},1fr)`;
     let howBig = size*size;
+
     for (let i = 0; i < howBig; i++) {
         let container = document.createElement("div");
-        container.addEventListener("mouseover", () => {
-            container.style.backgroundColor = `black`
-        })
+        container.classList.add("squares")
         grid.insertAdjacentElement("beforeend",container);
     }
 }
@@ -33,9 +42,31 @@ let changeG = document.querySelector("#change");
 changeG.addEventListener("click", () => {
     let newSize = prompt("What's the new size?");
     if (newSize <= 100 && newSize >= 2) {
-        newGrid(newSize);
+        newGrid(newSize, "black");
     }
     else {
         alert("Size must ve between 2 and 100 :D");
     }
 })
+
+//Draw
+
+let colorBar = document.querySelector("#colorChange");
+colorBar.addEventListener("change", (e) => {
+    newColor = e.target.value;
+})
+
+function draw(color = "black") {
+    let grid = document.querySelectorAll(".squares");
+    grid.forEach((grid) => {
+        grid.addEventListener("mouseover", (e) => {
+            grid.backgroundColor = "black"
+        })
+    })
+}
+
+function startDrawing(size = 16) {
+    clearGrid();
+    createGrid(size);
+    draw("black");
+}
